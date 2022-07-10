0fd516fca725c4dec58e0a028282179344590c21
Tran Dat <dattran1232003@gmail.com>
2022-07-10 13:40:57 +0700

    adding hash password & test functions

---
+++ util/password_test.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	// test HashPassword
+	password := RandomString(8)
+	hashedPassword1, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword1)
+
+	// test CheckPassword: correct
+	err = CheckPassword(password, hashedPassword1)
+	require.NoError(t, err)
+
+	// test CheckPassword: wrong
+	wrongPassword := password + RandomString(2)
+	err = CheckPassword(wrongPassword, hashedPassword1)
+	require.Error(t, err)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	// test HashPassword twice (two hash strings shouldn't be the same)
+	hashedPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword2)
+	require.NotEqual(t, hashedPassword1, hashedPassword2)
+}
